f5dff152bfa738747dbbf693dfd60f0d22dd66f6
AdrianaMargiotta <adrianamargiotta@protonmail.com>
2020-10-20 15:17:51 +0200

    struttura JS

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//Descrizione: chiedere all'utente il numero di chilometri che vuole percorrere e l'età. Sulla base di ciò calcolare il prezzo del biglietto
+
+//INPUT
+  //l'utente inserisce i kilometri che vuole percorrere
+
+
+  //l'utente inserisce l'età
+
+
+//OPERAZIONE
+  //calcolo prezzo del biglietto intero
+
+
+  //calcolo prezzo del biglietto per minorenni (20%)
+
+
+  //calcolo prezzo del biglietto per over 65 (40%)
+
+
+//OUTPUT
+//stabilire se l'utente ha diritto ad una percetuale di sconto
